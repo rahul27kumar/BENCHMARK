54012c4b3b2148212a227f22c761cd598ff7fe95
Nitesh Mahajan <nitesh.mahajan@seagate.com>
2020-08-07 06:53:14 +0000

    Modified installs3bench

--- s3bench_basic/installS3bench.sh
+++ s3bench_basic/installS3bench.sh
@@ -8,7 +8,7 @@ if [ ! -f "/root/go/bin/s3bench.old" ] ; then
    echo "Installing s3bench tools"
    cd ~; go get github.com/igneous-systems/s3bench
    cd /root/go/src/github.com/igneous-systems/s3bench; go build
-   mv /root/go/src/github.com/igneous-systems/s3bench/s3bench ~/go/bin/s3bench.old
+   yes | cp /root/go/src/github.com/igneous-systems/s3bench/s3bench ~/go/bin/s3bench.old
 else
    echo "S3benchmark Tools is already configured"
 fi
